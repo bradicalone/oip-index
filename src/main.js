2422d89ca27e030c2369431cb07c5dffdf5c2785
Sky Young <sky@alexandria.io>
2018-07-26 13:54:10 -0700

    Export Index properly

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Artifact from './Artifact'
 import ArtifactFile from './ArtifactFile'
 import Multipart from './Multipart'
-import Index from './index'
+import Index from './Index'
 
 module.exports = {
 	Artifact,
