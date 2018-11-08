e133fc85f424000480367fc72dc35057dd093201
orpheus <ryanachacon@gmail.com>
2018-11-08 12:10:00 -0800

    initial test for OIPElastic Index

--- test/OIPElastic.test.js
+++ test/OIPElastic.test.js
@@ -1,13 +1,13 @@
 import OIPElastic from '../src/OIPElastic'
-let net = new OIPElastic()
+let index = new OIPElastic()
 
 describe('OIPElastic', () => {
-	describe('Artifacts', () => {
-		it('GET Artifact', async () => {
-			let txid = 'f516ba2fa3eeceb154cf0606c98ab8640d45b773bbf8b1107a433f142c02e9b6'
-			let artifact = await net.getArtifact(txid)
-			console.log(artifact)
-
+	describe('OIPIndex', () => {
+		it('GET ResearchTomogramArtifact by txid | getArtifactByTXID()', async () => {
+			let txid = 'cc9a11050acdc4401aec3f40c4cce123d99c0f2c27d4403ae4a2536ee38a4716'
+			let artifact = await index.getArtifactByTXID(txid)
+			expect(artifact.isValid().success).toBeTruthy()
+			expect(artifact.getInternalArtifactType() === 'ResearchTomogram')
 		})
 	})
 })
