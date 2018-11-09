388858bde7797cc3c2e24d6d52e903cec5968fe0
orpheus <ryanachacon@gmail.com>
2018-11-08 16:16:02 -0800

    test: get Art by TXID

--- test/OIPElastic.test.js
+++ test/OIPElastic.test.js
@@ -1,13 +1,14 @@
 import OIPElastic from '../src/OIPElastic'
 let index = new OIPElastic()
 
-describe('OIPElastic', () => {
-	describe('OIPIndex', () => {
+describe('OIPIndex API', () => {
+	describe('OIPIndex API Functions', () => {
 		it('GET ResearchTomogramArtifact by txid | getArtifactByTXID()', async () => {
 			let txid = 'cc9a11050acdc4401aec3f40c4cce123d99c0f2c27d4403ae4a2536ee38a4716'
-			let artifact = await index.getArtifactByTXID(txid)
-			expect(artifact.isValid().success).toBeTruthy()
-			expect(artifact.getInternalArtifactType() === 'ResearchTomogram')
+			let response = await index.getArtifactByTXID(txid)
+			expect(response.success).toBeTruthy()
+			expect(response.artifact.getInternalArtifactType() === 'ResearchTomogram')
+		});
 		})
 	})
 })
