7337af4fbbaf536dfab635f1f054b529d9c9af70
orpheus <ryanachacon@gmail.com>
2018-11-12 08:52:24 -0800

    export Special-Type Artifacts

---
+++ src/Artifacts/index.js
@@ -0,0 +1,14 @@
+import PropertyParty from './PropertyParty'
+import PropertySpatialUnit from './PropertySpatialUnit'
+import PropertyTenure from './PropertyTenure'
+import ResearchTomogram from './ResearchTomogram'
+
+//this file is used to export all Specialized Artifact es6 classes (and not the basic generic Artifact class
+//which is exported in src/main.js
+
+export default {
+	PropertyTenure,
+	PropertySpatialUnit,
+	PropertyParty,
+	ResearchTomogram
+}
