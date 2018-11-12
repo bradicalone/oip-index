e443006717575fd1fd0e5970a3c066ca87ebc77b
orpheus <ryanachacon@gmail.com>
2018-11-12 08:55:46 -0800

    create OIPIndex with elastic endpoint

--- src/OIPIndex.js
+++ src/OIPIndex.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-import Artifact from './Artifact';
-import Multipart from './Multipart'
-
+import FloDataTX from './OIPComponents/FloDataTX'
+import ArtifactPicker from './HelperFunctions/ArtifactPicker'
 
 /**
  * The Transaction ID on the Blockchain.
@@ -11,25 +10,46 @@ import Multipart from './Multipart'
  * @example <caption>Shortened TXID Reference</caption>
  * 8a83ec
  */
-const defaultOIPdURL = "https://snowflake.oip.fun/alexandria/v2";
-class Index {
+
+const hydrateArray = (artifacts) => {
+	let tmpArray = []
+	for (let art of artifacts) {
+		tmpArray.push(ArtifactPicker(art))
+	}
+	return tmpArray
+}
+
+const hydrateFloDataTX = (floDataTXs) => {
+	let tmpArray = []
+	for (let data of floDataTXs) {
+		tmpArray.push(new FloDataTX(data))
+	}
+	return tmpArray
+}
+	//ToDo: change to 'https' when ready
+const defaultOIPdURL = "http://snowflake.oip.fun:1606";
+
+class OIPIndex {
 	/**
 	 * Spawn a new OIP Index with a specific OIPd URL
 	 * @param  {Object} [settings] - The Settings to use for the Index
 	 * @param {string} [settings.OIPdURL="https://snowflake.oip.fun/alexandria/v2"] [description]
 	 * @return {Index}
 	 */
-	constructor(settings){
+	constructor(settings) {
 		if (settings && settings.OIPdURL) {
 			this.setOIPdURL(settings.OIPdURL)
 		} else this.setOIPdURL(defaultOIPdURL)
 	}
 
-	setOIPdURL(OIPdURL){
+	setOIPdURL(OIPdURL) {
 		this.url = OIPdURL;
 
-		this.network = new axios.create({
-			baseURL: this.url
+		this.index = new axios.create({
+			baseURL: this.url,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+			},
 		})
 	}
 
@@ -38,378 +58,187 @@ class Index {
 	}
 
 	getNetwork() {
-		return this.network
+		return this.index
 	}
 
 	/**
 	 * Search The Index
-	 * @param  {string}  searchFor  - The string you wish to search for in the Index
-	 * @param  {String}  [searchOn="*"] - The "node" you wish to search on
-	 * @param  {Boolean} [searchLike=true] - Should we search similar (like) values, or only exact values
-	 * @param  {String}  [protocol="media"] - Which type we wish to get back
-	 * @return {Promise<Artifact|Publisher|Platform|Influencer|Autominer|AutominerPool>} Returns a Promise that will resolve to the hydrated object or reject on error
+	 * @param {string} query - your search query
+	 * @param {number} [limit=100] - max num of results
+	 * @return {Promise<Object>}
+	 * //return example
+	 * {success: true, artifacts: artifacts>}
+	 *
+	 * //or error
+	 * {success: false, error: error}
 	 */
-
-	//@ToDo::Convert to Elastic Search later
-	async search(searchFor, searchOn = "*", searchLike = true, protocol = "media"){
-		if (!searchFor)
-			throw new Error("searchFor is a required parameter!")
-
-		var options = {
-			searchFor,
-			searchOn,
-			searchLike,
-			protocol
+	async search(query, limit) {
+		if (typeof query !== 'string') {
+			return {success: false, error: `'query' must be of type string`}
 		}
-
-		var response = await this.network.post("/search", options)
-
-		return response.data
+		let res;
+		try {
+			res = await this.index.get(`/artifact/search`, {
+				params: {
+					q: query
+				},
+				limit
+			})
+		} catch (err) {
+			return {success: false, error: err}
+		}
+		if (res && res.data) {
+			let artifacts = res.data.results
+			if (artifacts.length === 0)
+				return {success: false, error: 'No artifacts found', response: res.data}
+			return {success: true, artifacts: hydrateArray(artifacts)}
+		} else {
+			return {success: false, error: 'No data returned from axios request', response: res}
+		}
 	}
 
-	//@ToDo::----------------Publishers and Artifacts-----------------------------------------------
-
 	/**
-	 * Get a specific Artifact from the Index (if searching on multipart, txid must be of the first multipart)
-	 * @param  {TXID} txid - The TXID of the Artifact you wish to get
-	 * @return {Promise<Artifact> | Promise<Object>} Returns a Promise that will resolve to an Artifact or an object containing an error
+	 * Get a specific Artifact from the Index by TXID
+	 * @param {string} txid  - transaction id of the artifact you wish to retrieve
+	 * @return {Promise<Object>} Returns a Promise that will resolve to an Artifact or an object containing an error
+	 * @example
+	 * //return example
+	 * {success: true, artifact: Artifact}
+	 *
+	 * //or error
+	 * {success: false, error: error}
 	 */
-	async getArtifact(txid){
+	async getArtifactByTXID(txid) {
+		let res
 		try {
-			let response = await this.network.get(`/artifact/get?id=${txid}`, {});
-			if (response && response.data) {
-				let tmpArt = new Artifact(response.data);
-				if (tmpArt.isValid().success)
-					return tmpArt
-			}
+			res = await this.index.get(`/artifact/get/${txid}`);
 		} catch (err) {
-			let error = {};
-			if (err.response && err.response.data) {
-				error.error = err.response.data || 'error';
-				error.status = `${err.response.status} ${err.response.statusText}` || 'status unknown';
-			} else {
-				error.error = "Error fetching artifact: could not get err response data"
-			}
-			return error;
+			return {success: false, error: err}
+		}
+		if (res && res.data) {
+			let resultArray = res.data.results
+
+			if (resultArray.length === 0) {
+				return {success: false, error: "No results found", response: res}
+			} else if (resultArray.length > 1) {
+				return {success: false, error: "Multiple artifacts found, possible collision", artifacts: resultArray}
+			} else return {success: true, artifact: ArtifactPicker(resultArray[0])}
+
+		} else {
+			return {success: false, error: 'No data returned from axios request', response: res}
 		}
 	}
 
 	/**
 	 * Get the Latest Artifacts published to the Index
-	 * @param  {number} [amount=50] - The amount of Artifacts you wish to receive
-	 * @return {Promise<Array.<Artifact>>} Returns a Promise that will resolve to an Array of Artifacts
+	 * @param {number} [limit=100] - The amount of artifacts you want returns ( max: 1000 )
+	 * @param {boolean} [nsfw=false] - not safe for work artifacts (don't be sick!)
+	 * @return {Promise<Object>}
+	 * @example
+	 * //return example
+	 * {success: true, artifacts: Array.<Artifact>}
+	 *
+	 * //or error
+	 * {success: false, error: error}
 	 */
-	async getLatestArtifacts(amount = 50){
+	async getLatestArtifacts(limit, nsfw = false) {
+		if (!limit)
+			limit = 100
+		if (limit > 1000)
+			limit = 1000
+		let res
 		try {
-			return await this.getArtifacts(null, null, amount, null, null);
-		} catch (err) {console.error(err)}
-	}
-
-	/**
-	 * Get the latest Artifacts from the Index
-	 * @param  {string} [type] - [The type of artifacts you wish to receive]
-	 * @param  {string} [subtype] - [The subtype of artifacts you wish to receive]
-	 * @param  {number} [numResults=100] - [The number of artifacts you wish to get back]
-	 * @param  {number} [page=1] - [Which 'page' you wish to search on (a page contains 50 or so Artifacts)]
-	 * @param {string} [publisher]  - []
-	 * @return {Promise<Array.<Artifact>>} Returns a Promise that will resolve to an Array of Artifacts
-	 */
-	async getArtifacts(type, subtype, numResults, page, publisher){
-		let p = page;
-		let nr = numResults || 50;
-		let t = type || "*";
-		if (typeof t === "string") t = t.toLowerCase();
-		let st = subtype || "*";
-		if (typeof t === "string") st = st.toLowerCase();
-		let pub = publisher || "";
-
-
-		if (page && isNaN(page) && page !== "*"){
-			p = 1;
-		} else if (page === "*") {
-			nr = "*";
-			p = 1;
+			res = await this.index.get(`/artifact/get/latest/${limit}`, {
+				params: {
+					nsfw
+				}
+			});
+		} catch (err) {
+			return {success: false, error: err}
 		}
 
-		try {
-			let response = await this.network.get(`/artifact/get/type?t=${t}&st=${st}&results=${nr}&page=${p}&pub=${pub}`);
-			let artifacts = [];
-			if (response && response.data && response.data.results) {
-				for (let art of response.data.results) {
-					let tmpArt = new Artifact(art);
-					if (tmpArt.isValid().success)
-						artifacts.push(tmpArt)
-				}
+		if (res && res.data) {
+			let artifacts = res.data.results
+			if (artifacts.length === 0)
+				return {success: false, error: 'No artifacts found', response: res.data}
+			if (artifacts.length > 0 && artifacts.length < limit)
+				return {success: true, artifacts: hydrateArray(artifacts), warning: 'Not all requested artifacts were found'}
+			if (artifacts.length === limit)
+				return {success: true, artifacts: hydrateArray(artifacts)}
+		} else {
+			return {success: false, error: 'No data returned from axios request', response: res}
+		}
+	}
+
+	/**
+	 * Get multiple Artifacts by their TXID
+	 * @param {Array.<string>} txids - an array of transaction IDs
+	 * @return {Promise<Object>}
+	 * @example
+	 * //return example
+	 * {success: true, artifacts: artifacts>}
+	 *
+	 * //or error
+	 * {success: false, error: error}
+	 */
+	async getArtifacts(txids) {
+		if (!Array.isArray(txids)) {
+			return {success: false, error: `'txids' must be an Array of transaction IDs`}
+		}
+		let artifacts = []
+		let errors = []
+		for (let txid of txids) {
+			let res
+			try {
+				res = await this.getArtifactByTXID(txid)
+			} catch (err) {
+				return {success: false, error: err}
 			}
-			return artifacts
-		} catch (err) {return err}
-	}
-
-	/**
-	 * Get a specific Publisher from the Index
-	 * @param  {TXID} txid - The TXID of the Publisher you wish to get
-	 * @return {Promise<Publisher>} Returns a Promise that will resolve to an Publisher or reject with an error
-	 */
-	async getPublisher(){
-		//@ToDo::add working API endpoint
-		try {
-			let response =  await this.network.get(`/artifact/get/publisher`).data;
-			return response.data
-		}catch (err) {return err}
-	}
-
-	/**
-	 * Get a all Publishers from Index
-	 * @return {} []
-	 */
-	async getAllPublishers() {
-		//@ToDo::add working API endpoint
-		try {
-			let response = await this.network.get(`/publisher/get/all`);
-			return response.data
-		}catch (err) {return err}
-	}
-
-	//@ToDo::----------------Influencers and Platforms----------------------------------------------
-
-	/**
-	 * Get a specific Platform from the Index
-	 * @param  {TXID} txid - The TXID of the Platform you wish to get
-	 * @return {Promise<Platform>} Returns a Promise that will resolve to an Platform or reject with an error
-	 */
-	async getPlatform(txid){
-		//@ToDo::add working API endpoint
-	}
-
-	/**
-	 * Get all Platformss from the Index
-	 * @return {Promise<Platforms>} []
-	 */
-	async getAllPlatforms(){
-		//@ToDo::add working API endpoint
-		try {
-			return await this.network.get(`/retailer/get/all`)
-		}catch (err) {return err}
-	}
-
-	/**
-	 * Get a specific Influencer from the Index
-	 * @param  {TXID} txid - The TXID of the Influencer you wish to get
-	 * @return {Promise<Influencer>} Returns a Promise that will resolve to an Influencer or reject with an error
-	 */
-	async getInfluencer(txid){
-		//@ToDo::add working API endpoint
-	}
-
-	/**
-	 * Get a all Influencers from the Index
-	 * @return {Promise<Influencer>} []
-	 */
-	async getAllInfluencers(){
-		//@ToDo::add working API endpoint
-		try {
-			return await this.network.get(`/promoter/get/all`)
-		}catch (err) {return err}
-	}
-
-	//@ToDo::----------------Autominers and Pools---------------------------------------------------
-
-	/**
-	 * Get a specific Autominer from the Index
-	 * @param  {TXID} txid - The TXID of the Autominer you wish to get
-	 * @return {Promise<Autominer>} Returns a Promise that will resolve to an Autominer or reject with an error
-	 */
-	async getAutominer(txid){
-		//@ToDo::add working API endpoint
-	}
-
-	/**
-	 * Get a all Autominers from the Index
-	 * @return {Promise<Autominer>} Returns a Promise that will resolve to an Autominer or reject with an error
-	 */
-	async getAllAutominers(){
-		//@ToDo::add working API endpoint
-		try {
-			return await this.network.get(`/autominer/get/all`)
-		}catch (err) {return err}
-	}
-
-	/**
-	 * Get a specific AutominerPool from the Index
-	 * @param  {TXID} txid - The TXID of the AutominerPool you wish to get
-	 * @return {Promise<AutominerPool>} Returns a Promise that will resolve to an AutominerPool or reject with an error
-	 */
-	async getAutominerPool(txid){
-		//@ToDo::add working API endpoint
-	}
-
-	/**
-	 * Get a all Autominers from the Index
-	 * @return {Promise<Autominer>} Returns a Promise that will resolve to an Autominer or reject with an error
-	 */
-	async getAllAutominerPools(){
-		//@ToDo::add working API endpoint
-		try {
-			return await this.network.get(`/autominerpool/get/all`)
-		}catch (err) {return err}
-	}
-
-	//@ToDo::----------------Flo Data and Multipart-------------------------------------------------
-
-	/**
-	 * Get a the floData of a specific Transaction
-	 * @param  {TXID} txid - The TXID you wish to get the floData of
-	 * @return {Promise<String>} Returns a Promise that will resolve to a floData string or reject with an error
-	 */
-	async getFloData(txid){
-		try {
-			let response = await axios.get(`https://livenet.flocha.in/api/tx/${txid}`)
-			return response.data.floData
-		} catch (err) {
-			let error = {};
-			if (err.message) {error.message = err.message}
-			if (error.response && error.response.data) {
-				error.error = err.response.data || 'error';
-				error.status = `${err.response.status} ${err.response.statusText}` || 'status unknown';
-			}
-			return error;
+			if (res.success) artifacts.push(res.artifact)
+			else errors.push(res.artifact)
+		}
+		if (errors.length > 0) {
+			return {success: false, error: 'Not [all artifacts] found', errors, artifacts}
+		} else {
+			return {success: true, artifacts: artifacts}
 		}
 	}
 
 	/**
 	 * Search all the floData published into the Flo Blockchain, this is provided by a connection to an OIPd server
-	 * @param  {Object|string} options - [the string you wish to search or an object with 'search' and/or 'page' properties]
-	 * @return {Promise<Array.<Object>>} Returns a Promise that will resolve to an Array of objects containing the transaction hash and message matched
+	 * @param {string} query - your search query
+	 * @param {number} [limit] - max num of results
+	 * @return {Promise<Object>}
+	 * //return example
+	 * {success: true, floData: Array.<FloDataTXs>}
+	 *
+	 * //or error
+	 * {success: false, error: error}
 	 */
-	async searchFloData(options){
-		if (!options)
-			throw new Error("Must provide a string or object")
-
-		if (typeof options === "string"){
-			options = {
-				search: options
-			}
+	async searchFloData(query, limit) {
+		if (typeof query !== 'string') {
+			return {success: false, error: `'query' must be of type string`}
 		}
-
-		if (!options.page)
-			options.page = 0;
-
-		if (!options["results-per-page"])
-			options["results-per-page"] = 30;
+		let res;
 		try {
-			let response = await this.network.post(`/searchTxComment`, options)
-			return response.data
-		} catch (err) {return err}
-	}
-
-	/**
-	 * Build and get the multiparts 
-	 * @param  {string} txid - The TXID of a valid oip-multipart
-	 * @return {Promise<Array.<Multipart>|Artifact>} Returns a Promise that will resolve to an Array of Multiparts, or a single Artifact if it is not Multiparts
-	 */
-	async getMultiparts(txid){
-		if (!txid || typeof txid !== "string" || txid.length === 0)
-			throw new Error("You must input a search txid!");
-
-		//@ToDO::   Step 1 - Get Artifact to set TXID
-		let artifact;
-		try {
-			artifact = await this.getArtifact(txid);
-		} catch (err) {console.error(err)}
-
-		let reqFloDataTXID = txid;
-		if (!artifact.error) {
-			if (txid && artifact && artifact.txid) {
-				if (txid.length <= 5) {
-					txid = artifact.txid;
-				}
-				reqFloDataTXID = artifact.txid;
-			}
+			res = await this.index.get(`/floData/search`, {
+				params: {
+					q: query
+				},
+				limit
+			})
+		} catch (err) {
+			return {success: false, error: err}
 		}
+		if (res && res.data) {
+			let txs = res.data.results
+			return {success: true, floData: hydrateFloDataTX(txs)}
 
-		//@ToDO::   Step 2 - Get FloData with TXID
-		let floData;
-		try {
-			floData = await this.getFloData(reqFloDataTXID);
-		} catch (err) {throw new Error(`Couldn't get floData to hydrate multipart: ${err}`)}
-
-		//@ToDO::   Step 3 - Create FirstMP with FloData from step 2
-		let results, matched = [], existingParts = [];
-
-		let firstMp = new Multipart(floData, reqFloDataTXID);
-		if (firstMp.is_valid) {
-			matched.push(firstMp)
-			existingParts.push(firstMp.getPartNumber())
 		} else {
-			throw firstMp.invalid_error || firstMp.isValid().message
-		}
-
-		let firstPartTXID = txid;
-		if (!firstMp.is_first_part) {
-			firstPartTXID = firstMp.getFirstPartTXID()
-			try {
-				let artifact = await this.getArtifact(firstPartTXID.substr(0,10))
-				let floData = await this.getFloData(artifact.txid)
-			} catch (err) {console.error(err)}
-			let tmpMp = new Multipart(floData, artifact.txid);
-			if (tmpMp.is_valid) {
-				matched.push(tmpMp)
-				existingParts.push(tmpMp.getPartNumber())
-			} else {throw new Error(`First multipart is not a valid multipart: ${tmpMp.isValid().message}`)}
-		}
-
-		//@ToDO::   Step 5 - Get the rest of the MPs
-		let searchFloTXID = (firstPartTXID.length > 10) ? firstPartTXID.substr(0,10) : firstPartTXID
-
-		let searchOps = {
-			search: searchFloTXID,
-			page: 0
-		}
-
-		async function findRemainingMultiparts(searchOps, _this) {
-			try {
-				results = await _this.searchFloData(searchOps);
-				if (results === null) {
-					return null
-				}
-			} catch (err) {console.error(err); return null}
-
-			if (results) {
-				for (let mp of results) {
-					let tmpMp = new Multipart(mp.Message, mp.Hash);
-
-					let trimLength = firstPartTXID.length
-					// Take whichever is shorter
-
-					if (tmpMp.getFirstPartTXID().length < trimLength && tmpMp.getFirstPartTXID().length > 0) {
-						trimLength = tmpMp.getFirstPartTXID().length
-					}
-
-					if (firstPartTXID.substr(0, trimLength) === tmpMp.getFirstPartTXID().substr(0, trimLength)) {
-						if (!existingParts.includes(tmpMp.getPartNumber())) {
-							matched.push(tmpMp)
-							existingParts.push(tmpMp.getPartNumber())
-						}
-					}
-				}
-				results = [];
-			}
-		}
-
-		while (matched.length - 1 < firstMp.getTotalParts()) {
-			const _this = this;
-			let stop = await findRemainingMultiparts(searchOps, _this)
-
-			if ((matched.length - 1 === firstMp.getTotalParts()) || stop === null) {
-				let ret = new Array(firstMp.getTotalParts()+1)
-				for (let i = 0; i < matched.length; i++) {
-					ret[matched[i].getPartNumber()] = matched[i]
-				}
-				return ret
-			}
-			searchOps.page++
+			return {success: false, error: 'No data returned from axios request', response: res}
 		}
 	}
+
 }
 
-module.exports = Index;
+export default OIPIndex;
