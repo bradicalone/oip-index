bf1b2957ce4e8e87a44de785baed7fed8b89bbc2
orpheus <ryanachacon@gmail.com>
2018-11-12 08:52:47 -0800

    export new Artifact classes and funcs at entry point

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import Artifact from './Artifact'
-import ArtifactFile from './ArtifactFile'
-import Multipart from './Multipart'
+// import Artifact from './Artifacts/Artifact'
+// import {ARTIFACTS} from "./Artifacts";
+import Artifact from './Artifacts/Artifact'
+import ArtifactFile from './OIPComponents/ArtifactFile'
+import Multipart from './OIPComponents/Multipart'
 import Index from './OIPIndex'
+import ArtifactPicker from './HelperFunctions/ArtifactPicker'
+
 
 module.exports = {
+	// ...ARTIFACTS,
 	Artifact,
 	ArtifactFile,
+	ArtifactPicker,
 	Multipart,
 	Index
 }
