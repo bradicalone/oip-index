d3d66b2af8a7d119ea22a7cf65d5e6652f25fb8e
orpheus <ryanachacon@gmail.com>
2018-11-13 16:45:49 -0800

    change test name

--- test/OIPIndex.test.js
+++ test/OIPIndex.test.js
@@ -4,7 +4,7 @@ let index = new OIPIndex()
 
 describe('OIPIndex API', () => {
 	describe('OIPIndex API Functions', () => {
-		it('GET ResearchTomogram by txid | getArtifactByTXID()', async () => {
+		it('GET Artifact by TXID | getArtifactByTXID()', async () => {
 			let txid = 'cc9a11050acdc4401aec3f40c4cce123d99c0f2c27d4403ae4a2536ee38a4716'
 			let response = await index.getArtifactByTXID(txid)
 			expect(response.success).toBeTruthy()
