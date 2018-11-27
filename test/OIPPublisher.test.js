e8a10692ccce2df2228a3c622733438feaddb82e
orpheus <ryanachacon@gmail.com>
2018-11-27 13:45:23 -0800

    OIPPublisher test jest skeleton

---
+++ test/OIPPublisher.test.js
@@ -0,0 +1,18 @@
+import bitcoin from 'bitcoinjs-lib'
+
+import {flo_testnet} from '../src/networks'
+import {isValidWIF} from '../src/util'
+import OIPPublisher from '../src/OIPPublisher'
+
+const network = flo_testnet.network
+
+// const keypair = bitcoin.ECPair.makeRandom({network})
+// const tmpWif = keypair.toWIF()
+// console.log(isValidWIF(tmpWif, network))
+
+const wif = 'cRVa9rNx5N1YKBw8PhavegJPFCiYCfC4n8cYmdc3X1Y6TyFZGG4B'
+const ECPair = bitcoin.ECPair.fromWIF(wif, network)
+const p2pkh = bitcoin.payments.p2pkh({pubkey: ECPair.publicKey, network}).address
+
+describe(`OIP Publisher`, () => {
+})
