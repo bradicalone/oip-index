9919c422cd9f5d86f82e55d95c71f0eebe0b291e
orpheus <ryanachacon@gmail.com>
2018-11-26 16:05:00 -0800

    build tx hex func

--- src/OIPPublisher.js
+++ src/OIPPublisher.js
@@ -87,6 +87,69 @@ class OIPPublisher {
 		}
 	}
 
+	async buildTX(floData) {
+		let selected
+		try {
+			selected = await this.buildInputsAndOutputs(floData)
+		} catch (err) {
+			throw new Error(`Failed to build inputs and outputs: ${err}`)
+		}
+
+		// console.log('selected: ', selected)
+		let {inputs, outputs, fee} = selected
+
+		// inputs and outputs will be undefined if no solution was found
+		if (!inputs || !outputs) {
+			throw new Error("No Inputs or Outputs selected! Fail!")
+		}
+
+		let txb = new bitcoin.TransactionBuilder(this.network)
+
+		txb.setVersion(this.coininfo.txVersion) //1: w/o floData, 2: w/ floData
+
+		inputs.forEach(input => txb.addInput(input.txId, input.vout))
+
+		// Check if we are paying to ourself, if so, merge the outputs to just a single output.
+		// Check if we have two outputs (i.e. pay to and change)
+		if (outputs.length === 2) {
+			// If the first input is sending to the from address, and there is a change output,
+			// then merge the outputs.
+			if (outputs[0].address === this.p2pkh && !outputs[1].address) {
+				let totalToSend = outputs[0].value + outputs[1].value
+				outputs = [{
+					address: this.p2pkh,
+					value: totalToSend
+				}]
+			}
+		}
+
+		outputs.forEach(output => {
+			if (!output.address) {
+				throw new Error(`Missing output address on line #164: ${outputs}`)
+			}
+			txb.addOutput(output.address, output.value)
+		})
+
+		let extraBytes = this.coininfo.getExtraBytes({floData})
+
+		for (let i in inputs) {
+			if (this.p2pkh !== inputs[i].address) throw new Error(`Invalid inputs. Addresses don't match on line #170: ${inputs} & ${this.p2pkh}`)
+			sign(txb, extraBytes, parseInt(i), this.ECPair)
+		}
+
+		let builtHex
+
+		try {
+			builtHex = txb.build().toHex();
+		} catch (e) {
+			throw new Error("Unable to build Transaction Hex! \n" + e)
+		}
+
+		builtHex += extraBytes
+
+		return builtHex
+	}
+
 }
 
 export default OIPPublisher
