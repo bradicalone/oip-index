bb7699079c1bdc7652bbcba14877c11e03ab7ad6
orpheus <ryanachacon@gmail.com>
2018-11-27 13:30:56 -0800

    export OIPPublisher

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Artifact from './Artifacts/Artifact'
 import ArtifactFile from './OIPComponents/ArtifactFile'
 import Multipart from './OIPComponents/Multipart'
 import Index from './OIPIndex'
+import OIPPublisher from './OIPPublisher'
 
 module.exports = {
 	Artifact,
 	ArtifactFile,
 	Multipart,
-	Index
+	Index,
+	OIPPublisher
 }
