d1685094610f1cddaa000d5c3dc57fb688d81f60
orpheus <ryanachacon@gmail.com>
2018-11-29 14:29:11 -0800

    MultipartX test jest skeleton

---
+++ test/MultipartX.test.js
@@ -0,0 +1,8 @@
+import MultipartX from '../src/OIPComponents/MultipartX'
+import OIPIndex from '../src/OIPIndex'
+import Artifact from '../src/Artifacts/Artifact'
+let index = new OIPIndex()
+
+
+describe("MultipartX", () => {
+})
