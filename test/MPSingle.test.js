3d1a76fa2f79caa4c97ab8e7055fcbc73747f10e
orpheus <ryanachacon@gmail.com>
2018-11-29 11:51:49 -0800

    MPSingle test jest skeleton

---
+++ test/MPSingle.test.js
@@ -0,0 +1,10 @@
+import bitcoin from 'bitcoinjs-lib'
+import {verify} from 'bitcoinjs-message'
+import {flo_testnet} from '../src/networks'
+import MPSingle from '../src/OIPComponents/MPSingle'
+import OIPIndex from '../src/OIPIndex'
+
+const index = new OIPIndex()
+
+describe("MPSingle", () => {
+})
